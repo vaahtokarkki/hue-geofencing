40dd451d9a084cc4f3f2566d4f10f2a8e41b2fa2
Roni Lindholm <roni.lindholm@gmail.com>
2019-09-14 18:58:37 +0300

    Add is past sunset test

--- tests/test_sun.py
+++ tests/test_sun.py
@@ -22,12 +22,20 @@ def mock(mocker):
 
 def test_sun(mock):
     sun = Sun()
-    sunrise = datetime.utcnow() + timedelta(days=1)
-    sunrise = sunrise.replace(hour=3, minute=15, second=0, microsecond=0)
-    sunset = datetime.utcnow().replace(hour=17, minute=30, second=0, microsecond=0)
+    sunrise_mocked = datetime.utcnow() + timedelta(days=1)
+    sunrise_mocked = sunrise_mocked.replace(hour=3, minute=15, second=0, microsecond=0)
+    sunset_mocked = datetime.utcnow().replace(hour=17, minute=30, second=0, microsecond=0)
     timestamp_delta = datetime.utcnow() - sun.timestamp
 
-    assert sunrise == sun.sunrise
-    assert sunset == sun.sunset
+    assert sunrise_mocked == sun.sunrise
+    assert sunset_mocked == sun.sunset
     assert round(timestamp_delta.total_seconds()) == 0
     assert sun._scheduler is not None
+
+
+def test_is_past_sunset(mock):
+    sun = Sun()
+    sunset_mocked = datetime.utcnow().replace(hour=17, minute=30, second=0, microsecond=0)
+
+    expected_value = datetime.utcnow() > sunset_mocked
+    assert expected_value == sun.is_past_sunset()
