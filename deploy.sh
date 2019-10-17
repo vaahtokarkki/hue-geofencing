bbb36adaaddb02a90ca06c46daaf5439ba490907
Roni Lindholm <roni.lindholm@gmail.com>
2019-10-17 19:49:59 +0300

    Move docker build to deploy script
    This makes test run faster as there is no need to build docker image if
    test fails

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push vaahtokarkki/hue-geofencing
+docker build -t "$DOCKER_USERNAME"/"$DOCKER_REPO" .
+docker push "$DOCKER_USERNAME"/"$DOCKER_REPO"
