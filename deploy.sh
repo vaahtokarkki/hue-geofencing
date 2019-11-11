6eac2c48a674515a72051350a79404b17d542618
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-11 20:19:31 +0200

    Add default latest tag to docker image

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 touch .env # Fix docker compose build when missing env file
 docker-compose build
+docker tag "$DOCKER_REPO":"amd64" "$DOCKER_REPO":"latest"
 docker push "$DOCKER_USERNAME"/"$DOCKER_REPO"
