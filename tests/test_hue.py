17d649a0e12ec16a793d5e0d63c9fd761712b8d5
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-21 17:16:28 +0200

    Add tests for scene activation when scene is already activated

--- tests/test_hue.py
+++ tests/test_hue.py
@@ -6,15 +6,29 @@ from src.hue import Hue
 
 
 @pytest.fixture
-@patch('src.hue.Bridge')
-@patch('src.sun.threading')
-@patch('src.hue.Sun')
-def hue(bridge, monkeypatch, sun):
-    hue = Hue()
+def scene():
     scene = Mock()
     scene.name = "After sunset scene"
     scene.group = 1
     scene.scene_id = "scene_id"
+
+    lights = []
+    for _ in range(3):
+        light = Mock()
+        light.on = False
+        light.brightness = 200
+        lights.append(light)
+    scene.lights = lights
+    print(vars(scene))
+    return scene
+
+
+@pytest.fixture
+@patch('src.hue.Bridge')
+@patch('src.sun.threading')
+@patch('src.hue.Sun')
+def hue(bridge, monkeypatch, sun, scene):
+    hue = Hue()
     hue.bridge.scenes = [scene]
     return hue
 
@@ -36,6 +50,16 @@ def test_hue_arrive_after_sunset(hue):
 def test_hue_arrive_after_sunset_invalid_scene(hue):
     hue.sunset.is_past_sunset.return_value = True
     hue.bridge.scenes[0].name = "Invalid scene name"
+    hue.set_arrive()
+    hue.bridge.activate_scene.assert_not_called()
+
+
+def test_hue_arrive_when_scene_activated(hue):
+    hue.sunset.is_past_sunset.return_value = True
+
+    for scene_light in hue.bridge.scenes[0].lights:
+        scene_light.on = True
+
     hue.set_arrive()
     hue.bridge.activate_scene.assert_not_called()
 
