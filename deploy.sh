a04858a78091118361cfcc36d18563571faa8a80
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-21 17:28:28 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 touch .env # Fix docker compose build when missing env file
 docker-compose build
-docker tag "$DOCKER_REPO":"amd64" "$DOCKER_REPO":"latest"
+docker tag "$DOCKER_REPO":amd64 "$DOCKER_REPO":latest
 docker-compose push
-docker push "$DOCKER_REPO":"latest"
+docker push "$DOCKER_REPO":latest
