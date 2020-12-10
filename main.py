e14d83a908ae27e8a049c8b63442f5c4c82382a5
Roni Lindholm <roni.lindholm@gmail.com>
2020-12-10 15:57:20 +0200

    Add version info for debug

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from src.hue import Hue
 from src.network import Network
 from src.utils import setup_logger
 
+
+def version():
+    import logging
+    logger = logging.getLogger("main")
+    logger.info(f'Hue geofencing version {open("VERSION", "r").read()}')
+
+
 if __name__ == "__main__":
     setup_logger()
+    version()
     hue = Hue()
     network = Network(callback_leave=hue.set_leave_home, callback_join=hue.set_arrive)
